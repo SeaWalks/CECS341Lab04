02012b47b0a59faec1a021aa8cb548fc160d6183
SeaWalks <80789948+SeaWalks@users.noreply.github.com>
2021-04-06 02:15:59 -0700

    Update datapath.v
    Updated datapath for lab05. Now has provisions for I type instructions.

--- datapath.v
+++ datapath.v
@@ -3,23 +3,90 @@ module Datapath(
     input reset,
     output [31:0] Dout
     );
-    wire [31:0] pcAddress, pcAdder, instructionMem, rsOut, rtOut, writeData;
+    wire [31:0] pcAddOut, pcROut, iMemOut, rs, rt;
     wire [5:0] op, func;
-    wire [4:0] rsIn, rtIn, rdIn;
     wire [3:0] ALUCntl;
-    wire RegWrite;
-
-    assign op = instructionMem[31:26];
-    assign rsIn = instructionMem[25:21];
-    assign rtIn = instructionMem[20:16];
-    assign rdIn = instructionMem[15:11];
-    assign func = instructionMem[5:0];
+    wire RegWrite, Negative, Carry, Overflow, Zero;
     
-    PCRegister PC(.clk(clk), .reset(reset), .Din(pcAdder), .PC_out(pcAddress));
-    PCADD PCadd(.Din(pcAddress), .PC_add_out(pcAdder));
-    Instruction_Memory Imem(.Addr(pcAddress),.Inst_out(instructionMem));
-    control cnt(.Op(op), .Func(func), .RegWrite(RegWrite), .ALUCntl(ALUCntl));
-    regfile32 register(.clk(clk), .reset(reset), .D_En(RegWrite), .D_Addr(rdIn), .S_Addr(rsIn), .T_Addr(rtIn), .D(writeData), .S(rsOut), .T(rtOut));
-    alu V(.A(rsOut), .B(rtOut), .ALUCntrl(ALUCntl), .ALU_Out(Dout));
-    assign writeData = Dout;
+    //Declare new Wires
+    wire [31:0] SignExtended, ShiftTwo, BranchAdd;
+    wire [1:0] Branch;
+    wire RegDst, MemtoReg, MemRead, MemWrite, ALUSrc;
+    
+    //Output Wire for muxes
+    wire [5:0] RegDst_Mux;
+    wire [31:0] ALUSrc_Mux, MemtoReg_Mux, Branch_Mux;
+    //Mux Logic
+    assign RegDst_Mux = (RegDst == 1) ? iMemOut[15:11] : iMemOut[20:16];
+    assign ALUSrc_Mux = (ALUSrc == 1) ? SignExtend : rt;
+    assign MemtoReg_Mux = (RegDst == 1) ? MemRead : Dout;
+    // Branch Mux: if(Branch&&Zero)||(Branch&&!Zero) simplifies to if(branch)
+    assign Branch_Mux = (Branch == 1) ? BranchAdd : pcAddOut;
+      // if(Branch&&Zero)||(Branch&&!Zero) simplifies to if(branch)
+    
+    //Building Datapath
+    
+    PCRegister PC(
+        .clk(clk), 
+        .reset(reset), 
+        .Din(Branch_Mux), //PC Now takes input from Branch_Mux instead of pcAddOut 
+        .PC_out(pcROut));
+        
+    PCADD PCadd(
+        .Din(pcROut), 
+        .PC_add_out(pcAddOut));
+        
+    Instruction_Memory im(
+        .Addr(pcROut),
+        .Inst_out(iMemOut));
+        
+    control cnt(
+        .Op(iMemOut[31:26]), 
+        .Func(iMemOut[5:0]), 
+        .RegWrite(RegWrite), 
+        .ALUCntl(ALUCntl), 
+        .RegDst(RegDst));
+        
+    regfile32 rf(
+        .clk(clk), 
+        .reset(reset), 
+        .D_En(RegWrite), 
+        .D_Addr(RegDst_Mux), 
+        .S_Addr(iMemOut[25:21]), 
+        .T_Addr(iMemOut[20:16]), 
+        .D(MemtoReg_Mux), //Now takes input from MemtoReg_Mux
+        .S(rs), 
+        .T(rt));
+    
+    SignExtend Extender(
+        .Instruction(iMemOut[15:0]), 
+        .SignExtended(SignExtend)); 
+    
+    ShiftLeft2 Shifter(
+        .SignExtended(SignExtended), 
+        .Shifted(ShiftTwo));
+    
+    BranchAdd BranchAdder(
+        .ShiftLeft2(ShiftTwo), 
+        .PCAdd(pcAddOut), 
+        .BranchAddOut(BranchAdd));
+        
+    alu v(
+        .A(rs), 
+        .B(ALUSrc_Mux), //Now takes in input from MUX instead of RT; either RT or SignExtend output
+        .ALUCntl(ALUCntl),
+        .ALU_Out(Dout), 
+        .C(Carry), 
+        .V(Overflow), 
+        .N(Negative), 
+        .Z(Zero));
+        
+    DataMem DataMemory(
+        .clk(clk),               
+        .mem_wr(MemWrite),
+        .mem_rd(MemRead),
+        .addr(Dout),
+        .wr_data(rt), // "result from register file will be stored into the data memory" -> output of ALU is Dout
+        .rd_data(DataMem_out));
+        
 endmodule
